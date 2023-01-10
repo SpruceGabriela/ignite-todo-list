155df63a3fcc268ed463181c3b68d7e3ba64b586
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2023-01-10 00:08:37 -0300

    feat(header): implement new task component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
-import Logo from "../assets/Logo";
+import Logo from "../../assets/Logo";
+import NewTask from "../NewTask";
+
 import styles from "./Header.module.css";
 
 const Header = () => {
 	return (
 		<header className={styles.header}>
 			<Logo />
+			<NewTask />
 		</header>
 	);
 };
---
+++ src/components/NewTask/NewTask.tsx
@@ -0,0 +1,16 @@
+import Plus from "../../assets/Plus";
+import styles from "./NewTask.module.css";
+
+const NewTask = () => {
+	return (
+		<section className={styles.container}>
+			<input className={styles.newTask} type="text" placeholder="Adicione uma nova tarefa" />
+            <button className={styles.create}>
+                <span>Criar</span>
+                <Plus />
+            </button>
+		</section>
+	);
+};
+
+export default NewTask;
